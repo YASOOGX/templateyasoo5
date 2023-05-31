d0b67a4a4bc4d92bea74a88ca30ed5d743aef6c1
Yousef Mostafa <115595202+YASOOGX@users.noreply.github.com>
2023-05-31 23:24:09 +0300

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,8 @@
+let search=document.querySelector(".btnsearh")
+let toggle1=document.querySelector(".bksearch")
+
+
+search.onclick=function (){
+this.classList.toggle("none")
+toggle1.classList.toggle("none")
+}
